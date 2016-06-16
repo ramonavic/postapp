b706f407617d74cc9d66e61ef7f1f7a3ebc47d80
ramonavic <ramon.hofmeister@gmail.com>
2016-06-16 11:30:39 +0200

    Show post user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         has_many :posts
+
+
+  def handle
+    self.email.split('@')[0]
+  end
+
 end
